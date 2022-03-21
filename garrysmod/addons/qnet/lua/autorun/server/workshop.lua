f5eef0ee5561b13b2a610e75e91d8093d5ccab0c
queeek180 <queeek180@tobefilledbyoem.modem>
2022-03-21 14:43:11 +1100

    first commit

---
+++ garrysmod/addons/qnet/lua/autorun/server/workshop.lua
@@ -0,0 +1,75 @@
+-- == PLAYER MODELS == --
+-- sans
+resource.AddWorkshop("671023795")
+-- kermit
+resource.AddWorkshop("485879458")
+-- trollface
+resource.AddWorkshop("2203246561")
+-- breen
+resource.AddWorkshop("2725994387")
+-- dewritos
+resource.AddWorkshop("232539187")
+-- niko
+resource.AddWorkshop("2594828157")
+-- picolas
+resource.AddWorkshop("674702181")
+-- amogus
+resource.AddWorkshop("2227901495")
+-- spongbob
+resource.AddWorkshop("1526127534")
+-- miku
+resource.AddWorkshop("362803586")
+-- ralsei
+resource.AddWorkshop("1767752600")
+-- cj
+resource.AddWorkshop("157854748")
+-- teletubby
+resource.AddWorkshop("356485444")
+-- woody
+resource.AddWorkshop("156924206")
+-- steve
+resource.AddWorkshop("2530771116")
+-- zero punctuation
+resource.AddWorkshop("105292871")
+-- inkling
+resource.AddWorkshop("479265317")
+-- homer
+resource.AddWorkshop("380745554")
+-- neru
+resource.AddWorkshop("310360456")
+-- Cirno
+resource.AddWorkshop("855364383")
+-- flandre
+resource.AddWorkshop("1115666310")
+-- Remilia
+resource.AddWorkshop("1143840003")
+-- marisa
+resource.AddWorkshop("1671709491")
+-- komeiji
+resource.AddWorkshop("1609074487")
+-- reimu
+resource.AddWorkshop("1113892203")
+-- freeman
+resource.AddWorkshop("2510003856")
+-- satori
+resource.AddWorkshop("1927351939")
+-- udongein
+resource.AddWorkshop("1511722814")
+
+-- == TRAILS & PETS == --
+-- anime trails
+resource.AddWorkshop("639153306")
+-- niandra trails 01
+resource.AddWorkshop("161466256")
+-- niandra trails 02
+resource.AddWorkshop("171222056")
+-- niandra trails 03
+resource.AddWorkshop("200578293")
+-- fumos
+resource.AddWorkshop("2242232335")
+
+-- == ICONS, THEMES, ETC == --
+-- nord theme
+resource.AddWorkshop("2781198275")
+-- map icons
+resource.AddWorkshop("2094865730")
