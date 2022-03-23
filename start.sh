79b21ae4fc6f3b9bb00cc2aae2cb1eda9da1b8df
queeek180 <63887374+queeek180@users.noreply.github.com>
2022-03-24 03:11:47 +1100

    prop physics issues at higher tickrates?

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./srcds_run -game garrysmod +maxplayers 24 +map ttt_island_2013 +host_workshop_collection "2780692615" +sv_setsteamaccount "removed" -tickrate 128 +fps_max 128
+./srcds_run -game garrysmod +maxplayers 24 +map ttt_island_2013 +host_workshop_collection "2780692615" +sv_setsteamaccount "removed" -tickrate 66 +fps_max 66
